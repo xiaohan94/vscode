59f5b040ead8efbde5427b356bc75dcba17a5a9d
xiaohan94 <897007516@qq.com>
2020-04-19 23:43:19 +0800

    4/19 2次修改

--- index.js
+++ index.js
@@ -21,39 +21,6 @@ function reh(){
 function res(){
     document.getElementById("zhu2-left").style.display="block";
 }
-function jsq(){
-    var a=document.getElementById("txt1").value;//获取第一个输入框的值
-    var b=document.getElementById("txt2").value;//获取第二个输入框的值
-    var c=document.getElementById("set").value;//获取选择框的值
-    var num=document.getElementById("txt3").value;//声明计算结果变量
-    switch(c){
-        case '+': num=parseInt(a)+parseInt(b);
-        break;
-        case '-': num=parseInt(a)-parseInt(b);
-        break;
-        case '*': num=parseInt(a)*parseInt(b);
-        break;
-        case '/': num=parseInt(a)/parseInt(b);
-        break;
-    }
-    document.getElementById("txt3").value=num;//将计算结果输出
-}
-function clock(){
-    var time=new Date();
-    document.getElementById("clock").value=time;
-}
-var i=setInterval("clock()",1000);
-
-var num=0,j;
-function startCount(){
-document.getElementById("count").value=num;
-num=num+1;
-j=setTimeout("startCount()",1000);
-}
-function stopCount(){
-    clearTimeout(j);
-    alert("您的计时结束!");
-}
 function add1(){
     document.getElementById("xlcd1").style.display="block";
 }
